7aa8e4aa9776b6a8ff91591fbb6360fafb97016b
minseo <miseno1003@gmail.com>
2026-05-12 20:12:57 +0900

    터미널에 로그 찍히도록 수정

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
+from datetime import datetime
 
-# 페이지 설정
 st.set_page_config(
     page_title="OSS_PROJECT_3",
     page_icon="🐯",
@@ -9,12 +9,18 @@ st.set_page_config(
 )
 
 st.markdown("## 사용자 정보")
-name = st.text_input("이름", "김민서")
-age = st.number_input("나이", min_value=0, max_value=120, value=24)
-option = st.selectbox(
-    "좋아하는 프로그래밍 언어", ["c", "C++", "Python", "Java", "JavaScript"]
-)
-st.write(f"선택: {option}")
+
+user_id = st.text_input("아이디")
+password = st.text_input("비밀번호", type="password")
 
 if st.button("완료"):
+    print(f"---------------------------------------------------", flush=True)
+    print(f"[{datetime.now()}] 입력이 완료됨", flush=True)
+    print(f"[{datetime.now()}] 입력된 아이디: {user_id}", flush=True)
+    print(
+        f"[{datetime.now()}] 입력된 비밀번호: {password}",
+        flush=True,
+    )
+
+    st.success("입력이 완료되었습니다.")
     st.balloons()
